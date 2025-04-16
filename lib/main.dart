bf7bea2a5feacd2745ec0a28658ac8f4d36c6724
Totus tuo <totustuo@MacBook-Pro-de-Totus.local>
2025-04-16 14:22:05 -0600

    Start project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '/src/pages/page1_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Page1(),
+    );
+  }
+}
